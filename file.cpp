bb65c5344f81b7aca2315d2ec8e10172897cc1cf
absoluteecr <dariakvasivchak@gmail.com>
2023-11-14 23:15:04 +0200

    периший файл

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a, b;
+	cout << "\nВведіть A: ";
+	cin >> a;
+	cout << "\nВведіть B: ";
+	cin >> b;
+	(a%2==0 && b%2==0 || a%2<<0 && b%2<<0) ? cout << "\nA та В мають однакову парність" : cout << "\nA та В не мають однакову парність";
+}
