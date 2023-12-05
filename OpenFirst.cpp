b9f8981d350d907fb192598bae2bd364863faf3d
absoluteecr <dariakvasivchak@gmail.com>
2023-12-05 22:28:51 +0200

    first file

--- OpenFirst.cpp
+++ OpenFirst.cpp
@@ -8,10 +8,11 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int a, b, c;
+	cout << "Hello";
+	int a, b;
 	cout << "\nВведіть A: ";
 	cin >> a;
 	cout << "\nВведіть B: ";
 	cin >> b;
-	(a%2==0 && b%2==0 || a%2<<0 && b%2<<0) ? cout << "\nA та В мають однакову парність" : cout << "\nA та В не мають однакову парність";
+	cout << "\nА та В мають однакову парність?\t" << (a % 2 == 0 && b % 2 == 0 || a % 2 << 0 && b % 2 << 0);
 }
